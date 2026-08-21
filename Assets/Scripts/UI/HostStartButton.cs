046fc3489c2afe9c0f4b1f80c49dba7c83bb875b
AnonLight0o0 <iamreal9123@gmail.com>
2026-08-21 15:59:59 +0800

    .
    - Font changes
    - Added options for different levels

--- Assets/Scripts/UI/HostStartButton.cs
+++ Assets/Scripts/UI/HostStartButton.cs
@@ -5,11 +5,16 @@ using Photon.Pun;
 
 public class HostStartButton : MonoBehaviour
 {
+    [SerializeField] private string scene1 = "DodgeballMap1";
+    [SerializeField] private string scene2 = "DodgeballMap2";
+    [SerializeField] private string scene3 = "DodgeballMap3";
+    [SerializeField] private string scene4 = "DodgeballMap4";
+
     public void StartGame()
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.LoadLevel("DodgeballMap1");
+            PhotonNetwork.LoadLevel(scene1);
         }
     }
 }
